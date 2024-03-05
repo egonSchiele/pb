a51fbf55b8abe18ac557809d1b8fa83a333c75ce
Aditya Bhargava <adit@adit.io>
2024-03-05 08:02:02 -0600

    formatting

--- commands/init.ts
+++ commands/init.ts
@@ -7,21 +7,25 @@ export default async function init() {
       type: "input",
       name: "host",
       message: "Database host?",
+      format: (value) => value.trim(),
     },
     {
       type: "input",
       name: "port",
       message: "Database port?",
+      format: (value) => value.trim(),
     },
     {
       type: "input",
       name: "username",
       message: "Database username?",
+      format: (value) => value.trim(),
     },
     {
       type: "password",
       name: "password",
       message: "Database password?",
+      format: (value) => value.trim(),
     },
   ]);
 
